42730b863b3beaa4ca0ec956daf98d0cbaa3b13d
LZ27@proton.me <157941859+Newfies@users.noreply.github.com>
2024-02-03 20:31:24 -0600

    FInal Push For Niw

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
+var hasAudioPlayed = false;
+
 function playAudio() {
-    var audioElement = document.getElementById("aud");
-    audioElement.play();
+    if (!hasAudioPlayed) {
+        var audioElement = document.getElementById("aud");
+        var player = document.getElementById('s');
+        var apple = Math.floor(Math.random() * 10) + 1;  // Generate a random number between 1 and 10
+
+        if (apple === 1) {
+            player.setAttribute("src", "res/fixed-as.mp3");
+        } else if (apple === 2) {
+            player.setAttribute("src", "res/fixed-bi.mp3");
+        } else if (apple === 3) {
+            player.setAttribute("src", "res/fixed-diaf.mp3");
+        } else if (apple === 4) {
+            player.setAttribute("src", "res/fnaf.mp3");
+        } else if (apple === 5) {
+            player.setAttribute("src", "res/fixed-g.mp3");
+        } else if (apple === 6) {
+            player.setAttribute("src", "res/fixed-gl.mp3");
+        } else if (apple === 7) {
+            player.setAttribute("src", "res/fixed-iheay.mp3");
+        } else if (apple === 8) {
+            player.setAttribute("src", "res/fixed-ite.mp3");
+        } else if (apple === 9) {
+            player.setAttribute("src", "res/fixed-sc.mp3");
+        } else if (apple === 10) {
+            player.setAttribute("src", "res/fixed-stiutk.mp3");
+        }
+
+        audioElement.load();  // Reload the audio element with the new source
+        audioElement.play();  // Play the audio
+
+        hasAudioPlayed = true;  // Set the flag to indicate that the audio has been played
+    }
 }
