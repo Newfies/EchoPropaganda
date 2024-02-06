eae11c3a65a26d2e915821481e8054b270130a8d
LZ27@proton.me <157941859+Newfies@users.noreply.github.com>
2024-02-05 21:02:38 -0600

    fefef

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 // Boobies :3 (.) (.)
 // Rawr xD || UwU || OwO
 
+// And No, This Isn't AI Generated Either, I Am Actually More Awake Lol.
+
 // Variables
-const musiclist = ["x", "y", "z"];
-const player = document.getElementById("media");
+const musiclist = [
+    "aito.mp3",
+    "as.mp3",
+    "bal.mp3",
+    "Ball.mp3",
+    "beg.mp3",
+    "bi.mp3",
+    "biyj.mp3",
+    "CG.mp3",
+    "diaf.mp3",
+    "ec.mp3",
+    "fim.mp3",
+    "fnaf.mp3",
+    "gary.mp3",
+    "gl.mp3",
+    "hoaf.mp3",
+    "ibsl.mp3",
+    "iheay.mp3",
+    "ite.mp3",
+    "sc.mp3",
+    "stiutk.mp3"
+  ];
+  
+const player = document.getElementById("audio");
 const prewebs = document.getElementById("preweb");
 const enter = document.getElementById("enterbtn");
 const webs = document.getElementById("web");
@@ -21,13 +45,20 @@ function pickMusic() {
 
 function playMusic(RNG) {
     player.setAttribute("src", "music/" + musiclist[RNG]);
+    
 }
 
 // On Clickies ~(^w^)~
 enter.addEventListener("click", function(){
     prewebs.style.display = "none";
     webs.style.display = "block";
+    player.play();
 });
+
+// Joke Script
+function StealUserCookies(xyz){
+    return
+};
 
 // On Loaded
 document.addEventListener("DOMContentLoaded", loaded);
